e1cf99361b6d186b8df258745c89edca9ff314b7
NambiaGeorge <nambiageorge344@gmail.com>
2025-05-26 13:02:50 +0200

    Update prototype.ino

--- IIT/Codes/prototype.ino
+++ IIT/Codes/prototype.ino
@@ -1,27 +1,79 @@
-#include <PubSubClient.h>
+#include <SPI.h>
 #include <Ethernet.h>
+#include <PubSubClient.h>
+
+// Network settings (replace with your LAN details if available)
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-EthernetClient ethClient;
-PubSubClient client(ethClient);
-const char* mqtt_server = "192.168.1.100"; // Update with broker IP
+IPAddress ip(192, 168, 1, 177);
+const char* mqtt_server = "broker.hivemq.com"; 
+const int mqtt_port = 1883;
+const char* mqtt_client_id = "ArduinoFan1";
+const char* control_topic = "smartlab/fan1/control";
+const char* status_topic = "smartlab/fan1/status";
+
+
 const int relayPin = 7;
 
+// MQTT client
+EthernetClient ethClient;
+PubSubClient client(ethClient);
+
 void setup() {
+  // Initialize relay pin (or LED for simulation)
   pinMode(relayPin, OUTPUT);
-  Ethernet.begin(mac);
-  client.setServer(mqtt_server, 1883);
+  digitalWrite(relayPin, LOW);
+
+  // Start Ethernet (simulated in Tinkercad)
+  Ethernet.begin(mac, ip);
+
+  // Connect to MQTT broker
+  client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
-  client.subscribe("smartlab/fan1");
+
+  // Connect to MQTT
+  reconnect();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    if (client.connect(mqtt_client_id)) {
+      client.subscribe(control_topic);
+      client.publish(status_topic, "off"); 
+    } else {
+      delay(5000); 
+    }
+  }
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
+  // Parse MQTT message
   String message;
-  for (int i = 0; i < length; i++) message += (char)payload[i];
-  if (message == "on") digitalWrite(relayPin, HIGH); // LED on
-  else if (message == "off") digitalWrite(relayPin, LOW); // LED off
+  for (unsigned int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+
+  // Handle commands
+  if (message == "on") {
+    digitalWrite(relayPin, HIGH);
+    client.publish(status_topic, "on");
+  } else if (message == "off") {
+    digitalWrite(relayPin, LOW); 
+    client.publish(status_topic, "off");
+  }
 }
 
 void loop() {
-  if (!client.connected()) client.connect("ArduinoClient");
+  if (!client.connected()) {
+    reconnect();
+  }
   client.loop();
+
+  // Simulate time-based schedule
+  static unsigned long lastCheck = 0;
+  if (millis() - lastCheck > 60000) {
+
+    digitalWrite(relayPin, LOW);
+    client.publish(status_topic, "off");
+    lastCheck = millis();
+  }
 }
